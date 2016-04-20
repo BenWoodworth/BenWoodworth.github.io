2fa05d6592352205f86ab36d266b357984e2c52a
Ben Woodworth <ben@benwoodworth.net>
2016-04-20 18:16:51 -0400

    Copied website contents to github.io

---
+++ assets/js/template-loader.js
@@ -0,0 +1,16 @@
+function loadTemplate(template, curPage) {
+    var divTemplate = document.getElementById("template");
+    var curNavPage = ' class="cur-nav-page"';
+    var data = {
+        "content": divTemplate.innerHTML,
+        "mobile": /Mobi/.test(navigator.userAgent) ? "mobile" : "no-mobile",
+        "pageHome":    curPage == "home" ?    curNavPage : "",
+        "pageEdu":     curPage == "edu" ?     curNavPage : "",
+        "pageResume":  curPage == "resume" ?  curNavPage : "",
+        "pageContact": curPage == "contact" ? curNavPage : ""
+    };
+
+    $.get(template, function (templateContent) {
+        document.body.innerHTML = Mustache.to_html(templateContent, data);
+    });
+}
