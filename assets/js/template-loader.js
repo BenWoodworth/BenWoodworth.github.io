0ada6e9d3003dc89038a8f456067609c08b0d656
BenWoodworth <ben@benwoodworth.net>
2016-04-29 18:30:06 -0400

    Removed resume references.

--- assets/js/template-loader.js
+++ assets/js/template-loader.js
@@ -1,16 +1,1 @@
-function loadTemplate(template, curPage) {
-    var divTemplate = document.getElementById("template");
-    var curNavPage = ' class="cur-nav-page"';
-    var data = {
-        "content": divTemplate.innerHTML,
-        "mobile": /Mobi/.test(navigator.userAgent) ? "mobile" : "no-mobile",
-        "pageHome":    curPage == "home" ?    curNavPage : "",
-        "pageEdu":     curPage == "edu" ?     curNavPage : "",
-        "pageResume":  curPage == "resume" ?  curNavPage : "",
-        "pageContact": curPage == "contact" ? curNavPage : ""
-    };
-
-    $.get(template, function (templateContent) {
-        document.body.innerHTML = Mustache.to_html(templateContent, data);
-    });
-}
+function loadTemplate(template, curPage) {    var divTemplate = document.getElementById("template");    var curNavPage = ' class="cur-nav-page"';    var data = {        "content": divTemplate.innerHTML,        "mobile": /Mobi/.test(navigator.userAgent) ? "mobile" : "no-mobile",        "pageHome":    curPage == "home" ?    curNavPage : "",        "pageEdu":     curPage == "edu" ?     curNavPage : "",        "pageContact": curPage == "contact" ? curNavPage : ""    };    $.get(template, function (templateContent) {        document.body.innerHTML = Mustache.to_html(templateContent, data);    });}
